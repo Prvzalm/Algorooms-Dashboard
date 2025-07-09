ae0b8c36ae6da41c90852c78ac0c0dfc4070df29
Parvez <prvzalm69@gmail.com>
2025-07-09 19:31:08 +0530

    CORS workaround

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://core-api.algorooms.com/api",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json-patch+json",
     Accept: "*/*",
