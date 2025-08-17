989843cf84ab3f99a8e9747bfe93c2374139e54d
Parvez Alam <prvzalm69@gmail.com>
2025-08-17 21:39:47 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { queryClient } from "../queryClient";
 
 const axiosInstance = axios.create({
-  baseURL: "https://uat-core-api.algorooms.com/api",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json-patch+json",
     Accept: "*/*",
@@ -12,12 +12,12 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    // if (error.response?.status === 401) {
-    //   // localStorage.removeItem("token");
-    //   delete axiosInstance.defaults.headers.common["Authorization"];
-    //   // queryClient.clear();
-    //   window.location.href = "/";
-    // }
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      delete axiosInstance.defaults.headers.common["Authorization"];
+      queryClient.clear();
+      window.location.href = "/";
+    }
 
     return Promise.reject(error);
   }
