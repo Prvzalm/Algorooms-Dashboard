3c960fb11452903d7696e8caf9120ba80979244f
Shyam Sundar Vashishtha <shyamvashishtha640@gmail.com>
2025-08-17 18:09:58 +0530

    updated redirect

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -12,12 +12,12 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-      delete axiosInstance.defaults.headers.common["Authorization"];
-      queryClient.clear();
-      window.location.href = "/";
-    }
+    // if (error.response?.status === 401) {
+    //   // localStorage.removeItem("token");
+    //   delete axiosInstance.defaults.headers.common["Authorization"];
+    //   // queryClient.clear();
+    //   window.location.href = "/";
+    // }
 
     return Promise.reject(error);
   }
