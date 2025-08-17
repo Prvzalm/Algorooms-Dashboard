cb797d366a397a96ff8270a7a86664b2429a7d7d
Parvez <prvzalm69@gmail.com>
2025-08-17 21:34:55 +0530

    production

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,25 +2,25 @@ import axios from "axios";
 import { queryClient } from "../queryClient";
 
 const axiosInstance = axios.create({
-  baseURL: "https://uat-core-api.algorooms.com/api",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json-patch+json",
     Accept: "*/*",
   },
 });
 
-// axiosInstance.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       localStorage.removeItem("token");
-//       delete axiosInstance.defaults.headers.common["Authorization"];
-//       queryClient.clear();
-//       window.location.href = "/";
-//     }
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      delete axiosInstance.defaults.headers.common["Authorization"];
+      queryClient.clear();
+      window.location.href = "/";
+    }
 
-//     return Promise.reject(error);
-//   }
-// );
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://core-api.algorooms.com",
-  //       changeOrigin: true,
-  //       secure: true,
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://uat-api.algorooms.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
