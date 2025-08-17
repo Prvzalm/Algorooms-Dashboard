b22cda3562f68ce9726861d81975df26c0a5d49a
Parvez <prvzalm69@gmail.com>
2025-08-17 21:06:26 +0530

    axiosInstance

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -9,18 +9,18 @@ const axiosInstance = axios.create({
   },
 });
 
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-      delete axiosInstance.defaults.headers.common["Authorization"];
-      queryClient.clear();
-      window.location.href = "/";
-    }
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       localStorage.removeItem("token");
+//       delete axiosInstance.defaults.headers.common["Authorization"];
+//       queryClient.clear();
+//       window.location.href = "/";
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
 export default axiosInstance;
