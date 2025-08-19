581b3621a4ad634e3689bb916acceb027450bc79
Shyam Sundar Vashishtha <shyamvashishtha640@gmail.com>
2025-08-19 12:30:32 +0530

    updated proxy for amplify

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,7 +5,7 @@ import { queryClient } from "../queryClient";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token"));
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
